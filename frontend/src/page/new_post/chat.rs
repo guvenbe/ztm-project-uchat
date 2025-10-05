69197b1ae32a0c4c8d852631cb7084736ee644fe
Bora Guven <guvenbe@gmail.com>
2025-10-04 23:42:19 -0400

    post message

--- frontend/src/page/new_post/chat.rs
+++ frontend/src/page/new_post/chat.rs
@@ -1,24 +1,68 @@
 #![allow(non_snake_case)]
 
-use dioxus::html::{button, form};
 use crate::prelude::*;
 use dioxus::prelude::*;
+use serde::{Deserialize, Serialize};
+
+#[derive(Clone, Default, Debug, Serialize, Deserialize)]
+pub struct PageState {
+    pub message: String,
+    pub headline: String,
+}
+
+#[inline_props]
+pub fn MessageInput(cx: Scope, page_state: UseRef<PageState>) -> Element {
+    use uchat_domain::post::Message;
+
+    let max_chars = Message::MAX_CHARS;
+
+    let wrong_len = maybe_class!(
+        "err-text-color",
+        page_state.read().message.len() >= max_chars || page_state.read().message.is_empty()
+    );
+
+    cx.render(rsx! {
+        div {
+            label {
+                r#for: "message",
+                div {
+                    class: "flex flex-row items-baseline gap-2 justify-start",
+                    span { "Message" },
+                    span {
+                        class: "text-left {wrong_len}",
+                        "{page_state.read().message.len()}/{max_chars}",
+                    }
+                }
+            },
+            textarea {
+                class: "input-field",
+                id: "message",
+                rows: 5,
+                value: "{page_state.read().message}",
+                oninput: move |ev| {
+                    page_state.with_mut(|state| state.message = ev.data.value.clone());
+                }
+            }
+        }
+    })
+}
 
 pub fn NewChat(cx: Scope) -> Element {
+    let page_state = use_ref(cx, PageState::default);
+
     cx.render(rsx! {
         form {
             class: "flex flex-col gap-4",
             onsubmit: move |_| (),
             prevent_default: "onsubmit",
-            //message input
-            //headline input
+            MessageInput{ page_state: page_state.clone() },
+            // headline input
             button {
                 class: "btn",
                 r#type: "submit",
                 disabled: true,
                 "Post"
             }
-
         }
     })
 }
